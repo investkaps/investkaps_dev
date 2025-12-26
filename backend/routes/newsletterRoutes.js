8acbbeb51daa67edaad243e35e6c7ff6ff007499
Aimans30 <aiman.singh30@gmail.com>
2025-12-26 23:40:58 +0530

    fixed bug

--- backend/routes/newsletterRoutes.js
+++ backend/routes/newsletterRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const newsletterController = require('../controllers/newsletterController');
-const { requireAuth } = require('../middleware/authMiddleware');
+const { authenticateToken } = require('../middleware/authMiddleware');
 const { checkRole } = require('../middleware/roleAuth');
 
 router.post('/subscribe', newsletterController.subscribe);
 
 router.post('/unsubscribe', newsletterController.unsubscribe);
 
-router.get('/subscribers', requireAuth, checkRole(['admin']), newsletterController.getAllSubscribers);
+router.get('/subscribers', authenticateToken, checkRole('admin'), newsletterController.getAllSubscribers);
 
 module.exports = router;
